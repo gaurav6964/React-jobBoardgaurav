e4e1e1eb56157ddbf61ba79c67a012e4c58de925
gaurav6964 <kumargaurav.8603070@gmail.com>
2025-06-12 13:15:47 +0530

    my changes

---
+++ src/pages/HomeLayout.jsx
@@ -0,0 +1,10 @@
+const HomeLayout=()=>{
+    return (
+        <div>
+            <h1>This Homepage is beingopened through this file</h1>
+            <h2>Sorry, though the external file</h2>
+        </div>
+    )
+}
+
+export default HomepLayout;
