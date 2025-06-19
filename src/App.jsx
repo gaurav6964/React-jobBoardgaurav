01167232006d45aad492d298e7f3f470b322a1ec
gaurav6964 <kumargaurav.8603070@gmail.com>
2025-06-19 17:06:25 +0530

    final changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,122 @@
-import {createBrowserRouter,RouterProvider} from 'react-router-dom';
-import './App.css';
-import { HomeLayout, DashboardLayout, Landing } from './pages';
-import { Login,Register } from './components';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+import {
+  HomeLayout,
+  Landing,
+  Register,
+  Login,
+  DashboardLayout,
+  Error,
+  AddJob,
+  Stats,
+  AllJobs,
+  Profile,
+  Admin,
+  EditJob,
+} from './pages';
+
+import { action as registerAction } from './pages/Register';
+import { action as loginAction } from './pages/Login';
+import { loader as dashboardLoader } from './pages/DashboardLayout';
+import { action as addJobAction } from './pages/AddJob';
+import { loader as allJobsLoader } from './pages/AllJobs';
+import { loader as editJobLoader } from './pages/EditJob';
+import { action as editJobAction } from './pages/EditJob';
+import { action as deleteJobAction } from './pages/DeleteJob';
+import { loader as adminLoader } from './pages/Admin';
+import { action as profileAction } from './pages/Profile';
+import { loader as statsLoader } from './pages/Stats';
+import ErrorElement from './components/ErrorElement';
+
+export const checkDefaultTheme = () => {
+  const isDarkTheme = localStorage.getItem('darkTheme') === 'true';
+  document.body.classList.toggle('dark-theme', isDarkTheme);
+  return isDarkTheme;
+};
+
+checkDefaultTheme();
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+    },
+  },
+});
 
 const router = createBrowserRouter([
   {
-    path:'/',
+    path: '/',
     element: <HomeLayout />,
+    errorElement: <Error />,
     children: [
       {
         index: true,
-        element: <Landing />
+        element: <Landing />,
       },
       {
-        path:'/about',
-        element:(<div>
-                    <h1>This is About page open now</h1>
-                    <h1>What are you thinking today</h1>
-                </div>)
+        path: 'register',
+        element: <Register />,
+        action: registerAction,
       },
       {
-        path:'/login',
-        element: <Login/>
+        path: 'login',
+        element: <Login />,
+        action: loginAction(queryClient),
       },
       {
-        path: '/register',
-        element: <Register/>
+        path: 'dashboard',
+        element: <DashboardLayout queryClient={queryClient} />,
+        loader: dashboardLoader(queryClient),
+        children: [
+          {
+            index: true,
+            element: <AddJob />,
+            action: addJobAction(queryClient),
+          },
+          {
+            path: 'stats',
+            element: <Stats />,
+            loader: statsLoader(queryClient),
+            errorElement: <ErrorElement />,
+          },
+          {
+            path: 'all-jobs',
+            element: <AllJobs />,
+            loader: allJobsLoader(queryClient),
+            errorElement: <ErrorElement />,
+          },
+          {
+            path: 'profile',
+            element: <Profile />,
+            action: profileAction(queryClient),
+          },
+          {
+            path: 'admin',
+            element: <Admin />,
+            loader: adminLoader,
+          },
+          {
+            path: 'edit-job/:id',
+            element: <EditJob />,
+            loader: editJobLoader(queryClient),
+            action: editJobAction(queryClient),
+          },
+          { path: 'delete-job/:id', action: deleteJobAction(queryClient) },
+        ],
       },
-      {
-        path:'/dashboard',
-        element: <DashboardLayout/>
-      }
-    ]
-  }
-  
-])
+    ],
+  },
+]);
 
-function App() {
-  return <RouterProvider router={router}/>
+const App = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 };
-
 export default App;
